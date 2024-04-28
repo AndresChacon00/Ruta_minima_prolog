827479eff645f32846a666eb72d507eadbfa854e
gabrielmj23 <gabrielmj2312@gmail.com>
2024-04-28 14:59:52 -0400

    Hallar ruta mínima

--- vuelos.pl
+++ vuelos.pl
@@ -35,19 +35,9 @@ leer_csv(NombreArchivo) :-
     maplist(linea_a_vuelo, Lineas),
     print('Vuelos agregados con éxito').
 
-/* Base de conocimientos de pruebas 
-vuelo(pto_ordaz,maiquetia,7,8,5600,1,['l','m','mi','j','v','s','d']).
-vuelo(maiquetia,porlamar,9,10,4800,2,['l','m','mi','j','v']).
-vuelo(maiquetia,pto_ordaz,15,16,5300,3,['l','m','v']).
-vuelo(pto_ordaz,maracaibo,13,15,9000,4,['l','m','mi','j','v','s','d']).
-vuelo(maracaibo,maiquetia,17,18,5500,5,['l','m','mi','j','v','s','d']).
-vuelo(pto_ordaz,valencia,11,12.5,6700,6,['l','m','mi','j','v','s','d']).
-vuelo(maracaibo,valencia,8,8.75,4700,7,['l','m','mi','j','v','s','d']).
-vuelo(porlamar,maracaibo,2,3.5,5900,8,['l','m','mi','j','v','s','d']).
-vuelo(valencia,porlamar,17,18,4300,9,['l','s','d']).
+/*
+    Dada una Ciudad genere la información de todos los vuelos que parten de ella.
 */
-
-/* Dada una Ciudad genere la información de todos los vuelos que parten de ella. */
 print_vuelos_desde_ciudad([]).
 print_vuelos_desde_ciudad([(Destino,HoraSalida,HoraLlegada,Costo,Codigo,Frecuencia)|T]) :-
     format('Destino: ~w, HoraSalida: ~w, HoraLlegada: ~w, Costo: ~w, Codigo: ~w, Frecuencia: ~w~n', [Destino,HoraSalida,HoraLlegada,Costo,Codigo,Frecuencia]),
@@ -57,7 +47,9 @@ vuelos_desde_ciudad(Ciudad):-
     findall((Destino,HoraSalida,HoraLlegada,Costo,Codigo,Frecuencia), vuelo(Ciudad,Destino,HoraSalida,HoraLlegada,Costo,Codigo,Frecuencia), Resultado),
     print_vuelos_desde_ciudad(Resultado).
 
-/* Dada una Ciudad genere la información de todos los vuelos que llegan a ella. */
+/*
+    Dada una Ciudad genere la información de todos los vuelos que llegan a ella.
+*/
 print_vuelos_a_ciudad([]).
 print_vuelos_a_ciudad([(Ciudad,HoraSalida,HoraLlegada,Costo,Codigo,Frecuencia)|T]) :-
     format('Ciudad: ~w, HoraSalida: ~w, HoraLlegada: ~w, Costo: ~w, Codigo: ~w, Frecuencia: ~w~n', [Ciudad,HoraSalida,HoraLlegada,Costo,Codigo,Frecuencia]),
@@ -68,30 +60,62 @@ vuelos_a_ciudad(Destino) :-
     print_vuelos_a_ciudad(Resultado).
 
 /*
+    Funciones de apoyo para los siguientes requerimientos
+*/
+obtener_costo_ruta([(_, _, _, Costo)], Costo).
+obtener_costo_ruta([_|RestoDestinos], CostoFin) :-
+    obtener_costo_ruta(RestoDestinos, CostoFin).
+
+obtener_hora_salida_ruta([(_, Salida, _, _)|_], SalidaFin) :-
+    SalidaFin is Salida.
+
+obtener_hora_llegada_ruta([(_, _, Llegada, _)], LlegadaFin) :-
+    LlegadaFin is Llegada.
+obtener_hora_llegada_ruta([_|Resto], LlegadaFin) :-
+    obtener_hora_llegada_ruta(Resto, LlegadaFin).
+
+/*
     Dada dos ciudades (origen y destino), determine todas las rutas entre ellas (directas
     o con escala) que puedan hacerse en un día, indicando las ciudades involucradas y
     los costos asociados a cada una de las rutas
 */
-append([],L2,L2).
-append([H|T],L2,[H|L3]):-append(T,L2,L3).
-
 isElement(X, [X|_]).
 isElement(X, [_|T]):- isElement(X,T).
 
-rutas(Origen, Destino, Dia, HoraActual, CostoT, [(Destino, NuevoCostoT)]) :-
-    vuelo(Origen, Destino, HoraSalida, _, Costo, _, VuelosSemanales),
+rutas(Origen, Destino, Dia, HoraActual, CostoT, [(Destino, Salida, Llegada, NuevoCostoT)]) :-
+    vuelo(Origen, Destino, HoraSalida, HoraLlegada, Costo, _, VuelosSemanales),
     isElement(Dia, VuelosSemanales),
     HoraActual =< HoraSalida,
+    Salida is HoraSalida,
+    Llegada is HoraLlegada,
     NuevoCostoT is CostoT + Costo.
 
-rutas(Origen, Destino, HoraActual, CostoT, [(Escala, NuevoCostoT)|RestoDestinos]) :-
+rutas(Origen, Destino, Dia, HoraActual, CostoT, [(Escala, Salida, Llegada, NuevoCostoT)|RestoDestinos]) :-
     vuelo(Origen, Escala, HoraSalida, HoraLlegada, Costo, _, VuelosSemanales),
     isElement(Dia, VuelosSemanales),
     Escala \= Destino,
     HoraActual =< HoraSalida,
     NuevoHoraActual is HoraLlegada,
+    Salida is HoraSalida,
+    Llegada is HoraLlegada,
     NuevoCostoT is CostoT + Costo,
-    rutas(Escala, Destino, NuevoHoraActual, NuevoCostoT, RestoDestinos).
+    rutas(Escala, Destino, Dia, NuevoHoraActual, NuevoCostoT, RestoDestinos).
 
+/*
+    Dadas dos ciudades (origen y destino), determine la ruta de costo mínimo entre
+    ellas, indicando las ciudades involucradas, hora de salida, hora de llegada y costo
+*/
+min_costo([], CostoMin, RutaMinCosto, CostoMin, RutaMinCosto).
+min_costo([(Costo, Ruta)|RestoRutas], CostoActual, _, CostoMin, RutaMinCosto) :-
+    Costo < CostoActual,
+    min_costo(RestoRutas, Costo, Ruta, CostoMin, RutaMinCosto).
+min_costo([(Costo, _)|RestoRutas], CostoActual, RutaActual, CostoMin, RutaMinCosto) :-
+    Costo >= CostoActual,
+    min_costo(RestoRutas, CostoActual, RutaActual, CostoMin, RutaMinCosto).
 
-
+ruta_min_costo(Origen, Destino, Dia, RutaMinCosto, CostoMin, HoraSalida, HoraLlegada) :-
+    findall((Costo, Ruta), (rutas(Origen, Destino, Dia, 0, 0, Ruta), obtener_costo_ruta(Ruta, Costo)), Rutas),
+    Rutas = [(Costo, Ruta)|RestoRutas],
+    min_costo(RestoRutas, Costo, Ruta, CostoMin, RutaMinCosto),
+    obtener_hora_salida_ruta(RutaMinCosto, HoraSalida),
+    obtener_hora_llegada_ruta(RutaMinCosto, HoraLlegada).
